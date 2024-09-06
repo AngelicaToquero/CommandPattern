f9de5ceab1801dafe57f1cc1804dcc97d42b1c95
Angelica Toquero <152822754+AngelicaToquero@users.noreply.github.com>
2024-09-06 23:25:46 +0800

    Create CentralHub.java

---
+++ CentralHub.java
@@ -0,0 +1,10 @@
+public class CentralHub {
+  private Command command;
+
+public void setCommand(Command command){
+  this.command = command;
+}
+  public void pressButton() {
+    command.execute();
+  }
+}
