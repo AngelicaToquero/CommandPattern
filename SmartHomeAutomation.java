e73f1dd1fb448ab25c5b975825a8b9cc23f1900c
Angelica Toquero <152822754+AngelicaToquero@users.noreply.github.com>
2024-09-08 22:17:56 +0800

    Update SmartHomeAutomation.java

--- SmartHomeAutomation.java
+++ SmartHomeAutomation.java
@@ -1,6 +1,5 @@
 public class SmartHomeAutomation {
     public static void main(String[] args) {
-
         Light light = new Light();
         Thermostat thermostat = new Thermostat();
         MusicPlayer musicPlayer = new MusicPlayer();
@@ -8,12 +7,14 @@ public class SmartHomeAutomation {
         Command lightOn = new LightOnCommand(light);
         Command lightOff = new LightOffCommand(light);
         Command increaseTemp = new IncreaseTemperatureCommand(thermostat);
+        Command decreaseTemp = new DecreaseTemperatureCommand(thermostat);
         Command playMusic = new PlayMusicCommand(musicPlayer);
-        COmmand decreaseVolume = new DecreaseVolumeCommand(musicPlayer);
+        Command increaseVolume = new IncreaseVolumeCommand(musicPlayer);
+        Command decreaseVolume = new DecreaseVolumeCommand(musicPlayer);
 
-        CentralHub = new CentralHub();
+        CentralHub hub = new CentralHub();
 
-        //Test
+        // Test
         hub.setCommand(lightOn);
         hub.pressButton();
 
@@ -23,7 +24,13 @@ public class SmartHomeAutomation {
         hub.setCommand(increaseTemp);
         hub.pressButton();
 
+        hub.setCommand(decreaseTemp);
+        hub.pressButton();
+
         hub.setCommand(playMusic);
+        hub.pressButton();
+
+        hub.setCommand(increaseVolume);
         hub.pressButton();
 
         hub.setCommand(decreaseVolume);
